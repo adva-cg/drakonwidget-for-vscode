a8b59281e8fecb0cf3cc0a8750666eab959b15b6
Adadurov Vitaly <v.adadurov@smarthorizon.ru>
2025-03-26 22:52:33 +0500

    Начало переделки на разные файлы во вкладках

--- out/extension.js
+++ out/extension.js
@@ -45,164 +45,65 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.activate = activate;
 exports.deactivate = deactivate;
 const vscode = __importStar(require("vscode"));
-//import * as path from 'path';
-function activate(context) {
-    registerDrakonCommands(context); // Вынесено в отдельную функцию    
-}
-function registerDrakonCommands(context) {
-    // Добавляем команду "Drakon.OpenFile" 
-    const openFileDisposable = vscode.commands.registerCommand('Drakon.OpenFile', () => __awaiter(this, void 0, void 0, function* () {
-        // Логика открытия файла
-        const fileUri = yield vscode.window.showOpenDialog({
-            filters: { 'DRAKON Diagrams': ['drakon'] }
-        });
-        if (fileUri && fileUri[0]) {
-            const panel = vscode.window.createWebviewPanel('drakonWidget', 'DrakonWidget', vscode.ViewColumn.One, { enableScripts: true,
-                localResourceRoots: [vscode.Uri.file(context.extensionPath)]
-            });
-            const extPath = context.extensionPath;
-            const extPathUri = panel.webview.asWebviewUri(vscode.Uri.file(extPath + "/drakonwidget"));
-            panel.webview.html = getWebviewContent(extPathUri);
-            // Загрузка данных в WebView
-            const fileContent = yield vscode.workspace.fs.readFile(fileUri[0]);
-            const jsonString = new TextDecoder('utf-8').decode(fileContent);
-            var diagram = JSON.parse(jsonString);
-            // Добавляем id, если его нет
-            if (!diagram.hasOwnProperty('id')) {
-                diagram.id = fileUri[0].path;
+const DRAKON_EDITOR_VIEW_TYPE = 'drakonEditor';
+class DrakonEditorProvider {
+    constructor(context) {
+        this.context = context;
+    }
+    resolveCustomTextEditor(document, webviewPanel) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // Настройка Webview
+            webviewPanel.webview.options = {
+                enableScripts: true,
+                localResourceRoots: [this.context.extensionUri]
+            };
+            // Получаем URI для ресурсов
+            const resourcesUri = webviewPanel.webview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, 'drakonwidget'));
+            // Загружаем и обрабатываем HTML
+            webviewPanel.webview.html = yield this.getWebviewContent(resourcesUri);
+            // Загрузка данных диаграммы
+            try {
+                const content = document.getText();
+                const diagram = content ? JSON.parse(content) : { type: "drakon", items: {} };
+                webviewPanel.webview.postMessage({
+                    command: 'loadDiagram',
+                    diagram: diagram
+                });
             }
-            ;
-            // const content = await fs.readFile(filepath, 'utf-8');
-            // return JSON.parse(content);
-            //  new TextDecoder('utf-8').decode(fileContent)
-            panel.webview.postMessage({
-                command: 'loadDiagram',
-                diagram: diagram
-            });
-        }
-    }));
-    // Не забываем добавить в subscriptions!
-    context.subscriptions.push(openFileDisposable);
-    const disposable = vscode.commands.registerCommand('Drakon.Start', () => {
-        const panel = vscode.window.createWebviewPanel('drakonWidget', 'DrakonWidget', vscode.ViewColumn.One, {
-            enableScripts: true,
-            localResourceRoots: [vscode.Uri.file(context.extensionPath)]
+            catch (error) {
+                vscode.window.showErrorMessage(`Error loading diagram: ${error}`);
+            }
         });
-        const extPath = context.extensionPath;
-        const extPathUri = panel.webview.asWebviewUri(vscode.Uri.file(extPath + "/drakonwidget"));
-        panel.webview.html = getWebviewContent(extPathUri);
-    });
-    context.subscriptions.push(disposable);
+    }
+    getWebviewContent(resourcesUri) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const templatePath = vscode.Uri.joinPath(this.context.extensionUri, 'templates', 'editor.html');
+            let html = (yield vscode.workspace.fs.readFile(templatePath)).toString();
+            // Заменяем плейсхолдеры на реальные URI
+            html = html.replace(/\${extPathUri}/g, resourcesUri.toString());
+            return html;
+        });
+    }
 }
-function getWebviewContent(extPathUri) {
-    return `
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width,initial-scale=1" />
-    <title>DrakonWidget online demo</title>
-    <script>
-        const extensionBaseUri = "${extPathUri}";
-    </script>
-    <meta name="description" content="DrakonWidget online demo shows how to use DrakonWidget in your application. This demo is an online
-        mini-editor for drakon flowcharts that does not require registration." />
-        <meta property="og:title" content="DrakonWidget online demo">
-        <meta property="og:description" content="DrakonWidget online demo shows how to use DrakonWidget in your application. This demo is an online
-        mini-editor for drakon flowcharts that does not require registration.">
-        <meta property="og:image" content="${extPathUri}/images/drakon-widget-hero.png">
-    <link rel="shortcut icon" href="${extPathUri}/images/favicon.ico" />
-    <link rel="icon" type="image/png" href="${extPathUri}/images/favicon.png" />
-    <style>
-        *,
-        *:before,
-        *:after {
-            -webkit-box-sizing: border-box;
-            -moz-box-sizing: border-box;
-            box-sizing: border-box;
+DrakonEditorProvider.viewType = 'drakonEditor';
+function activate(context) {
+    // Регистрация провайдера
+    const provider = new DrakonEditorProvider(context);
+    context.subscriptions.push(vscode.window.registerCustomEditorProvider(DRAKON_EDITOR_VIEW_TYPE, provider, { supportsMultipleEditorsPerDocument: false }));
+    // Команда создания новой диаграммы
+    context.subscriptions.push(vscode.commands.registerCommand('drakon.newDiagram', () => __awaiter(this, void 0, void 0, function* () {
+        const uri = vscode.Uri.parse(`untitled:Untitled-${Date.now()}.drakon`);
+        yield vscode.commands.executeCommand('vscode.openWith', uri, DRAKON_EDITOR_VIEW_TYPE);
+    })));
+    // Команда открытия файла
+    context.subscriptions.push(vscode.commands.registerCommand('drakon.openFile', () => __awaiter(this, void 0, void 0, function* () {
+        const uris = yield vscode.window.showOpenDialog({
+            filters: { 'Drakon Diagrams': ['drakon'] }
+        });
+        if (uris && uris[0]) {
+            yield vscode.commands.executeCommand('vscode.openWith', uris[0], DRAKON_EDITOR_VIEW_TYPE);
         }
-    </style>
-    <link rel="stylesheet" href="${extPathUri}/styles/reset.css" />
-    <link rel="stylesheet" href="${extPathUri}/styles/main.css" />
-</head>
-
-<body>
-    <div id="main">
-        <div id="left-toolbar"></div>
-        <div id="editor-area"></div>
-        <div id="menu" class="main-menu shadow">
-            <div class="menu-block">
-                <div id="close-button" class="generic-button simple-button" style="padding-left:0px;">
-                    <img src="${extPathUri}/images/delete.png"><span>Close</span>
-                </div>
-                <div class="menu-header">DrakonWidget online demo</div>
-            </div>
-            <div class="menu-block">
-                <table style="width: 100%;">
-                    <tr>
-                        <td style="width: 60px;">
-                            <div><img class="menu-logo" src="${extPathUri}/images/dra2.png" /></div>
-                        </td>
-                        <td style="width: calc(100% - 60px); text-align: right; vertical-align: top;">
-                            <p><a href="https://github.com/stepan-mitkin/drakonwidget">Documentation</a></p>
-                            <p>
-                            <div id="create-diagram-button" class="generic-button default-button"
-                                style="margin-right:0px;">Create diagram</div>
-                            </p>
-                        </td>
-                    </tr>
-                </table>
-
-            </div>
-            <div class="menu-block">
-                <div class="menu-label">Select diagram</div>
-                <select id="diagrams-combobox" class="main-menu-combobox"></select>
-            </div>
-            <div class="menu-block">
-                <div id="delete-diagram-button" class="generic-button simple-button">Delete diagram</div>
-                <div id="duplicate-diagram-button" class="generic-button simple-button">Duplicate diagram</div>
-                <div id="set-diagram-json-button" class="generic-button simple-button"
-                    style="margin-right: 0px;">Diagram JSON...
-                </div>
-            </div>
-            <div class="menu-block">
-                <div class="menu-label">Color theme</div>
-                <select id="themes-combobox" class="main-menu-combobox"></select>
-            </div>
-            <div class="menu-block" style="height: 46px;">
-                <div id="set-theme-json-button" class="generic-button simple-button right-button"
-                    style="margin-right: 0px;">Theme JSON...</div>
-            </div>
-            <div class="menu-block">
-                <div class="menu-label">Editor mode</div>
-                <select id="modes-combobox" class="main-menu-combobox"></select>
-            </div>
-            <div class="menu-block">
-                <div id="reset-all-diagrams-button" class="generic-button bad-button">Reset all diagrams</div>
-            </div>
-            <div style="height: 10px"></div>
-        </div>
-        <div id="snack-root"></div>
-        <div id="question-root"></div>
-        <div id="popup-root"></div>
-
-    </div>
-    <script src="${extPathUri}/libs/drakonwidget.js"></script>
-    <script src="${extPathUri}/libs/mousetrap.min.js"></script>
-    <script src="${extPathUri}/libs/simplewidgets.js"></script>
-    <script src="${extPathUri}/js/examples.js"></script>
-    <script src="${extPathUri}/js/themes.js"></script>
-    <script src="${extPathUri}/js/main.js"></script>
-</body>
-
-</html>
-`;
+    })));
 }
 function deactivate() { }
-// Для CommonJS-совместимости (если требуется)
-module.exports = {
-    activate,
-    deactivate
-};
 //# sourceMappingURL=extension.js.map
