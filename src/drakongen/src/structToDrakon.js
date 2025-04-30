4f6bfa9c711f2fdecd0cde1a05b7a324e9b78061
Adadurov Vitaly <v.adadurov@smarthorizon.ru>
2025-04-30 22:19:16 +0500

    Рефакторинг

--- src/drakongen/src/structToDrakon.js
+++ src/drakongen/src/structToDrakon.js
@@ -1,14 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-function isObject(value) {
-  return typeof value === 'object' && value !== null;
-}
-
-function isArray(value) {
-  return Array.isArray(value);
-}
-
 function astToDrakon(astJson) {
   try {
     const ast = JSON.parse(astJson);
@@ -19,235 +11,203 @@ function astToDrakon(astJson) {
     const branchIds = {};
     let nextNodeId = 1;
 
+    // Стек для отслеживания родительских узлов и направлений
+    const parentStack = [];
+
+    function isObject(value) {
+      return typeof value === 'object' && value !== null;
+    }
+
+    function isArray(value) {
+      return Array.isArray(value);
+    }
+
     function processBranches() {
-      let iconsForDirection = [];
       if (ast.branches) {
         for (const branch of ast.branches) {
           const branchNodeId = String(nextNodeId++);
           const iconBranch = {
             type: "branch",
             branchId: branch.branchId,
+            id: branchNodeId
           };
           drakon.items[branchNodeId] = iconBranch;
           branchIds[branch.branchId] = branchNodeId;
           if (ast.branches.length !== 1) {
             iconBranch.content = branch.name;
           }
-          iconsForDirection = processObjects(drakon.items, branch.body, [{ item: iconBranch, dir: "one" }]);
+          parentStack.push({ item: iconBranch, dir: 'one' });
+          const firstIcon = processObjects(drakon.items, branch.body, true);
+          if (firstIcon) {
+            iconBranch.one = firstIcon.id;
+          }
+          parentStack.pop();
         }
       }
-      return iconsForDirection;
+      exit();
     }
 
-    function processObjects(items, objectsForProcessing, iconsForLink) {
-      let firstAddedIconId = null;
-      let firstIconsForLink = iconsForLink;
-
-      function processAddress(element) {
-        const targetBranch = ast.branches.find(b => b.name === element.content);
-        if (targetBranch) {
-          iconsForLink[0].item.oneBranchId = targetBranch.branchId;
-        }
-      }
-
-      function processLopp(element) {
-
-        // определяем икону входа в цикл
-        
-        iconsForLink = processObjects(items, element.body, iconsForLink);
-        
-      }
-
-      function addIcon(element) {
-        const iconId = String(nextNodeId++);
-        const icon = {
-          content: element.content,
-          type: element.type,
-          id: iconId
-        };
-        if (element.type === "loopend") {
-          icon.type = "arrow-loop";
-          icon.content = "";
-        }
-        items[iconId] = icon;
-        if (firstAddedIconId === null) {
-          firstAddedIconId = iconId;
-        }
-        return icon;
-      }
-
-      function linkIcon(icon) {
-        for (const itemDir of iconsForLink) {
-          itemDir.item[itemDir.dir] = icon.id;
-        }
-        let newLink = [];
-        icon.one = null;
-        newLink.push({ item: icon, dir: 'one' });
-        if (icon.type === 'question') {
-          icon.two = null;
-          newLink.push({ item: icon, dir: 'two' });
-        }
-
-        return newLink;
-      }
-
-      function processQuestion(icon, element) {
-        // икона question может быть
-        // case для select. В этом случае для первого кейса надо добавить сверху селект, а саму икону преобразовать в case
-        // условием для проверки цикла. В этом случае надо путь с break направить вниз (брейк может быть на любом уровне вложенности), а без - вправо
-        // ну или просто условием. Здесь просто обрабатываем yes и no
-        // также условие может содержать внутри себя (в content) вложенное условие, которое тоже надо обработать (просто добавить нужные если и поменять связи)
-
-        // требуется
-        // 1) для кейса надо знать, что это первая икона
-        // 2) для цикла - надо знать начальную икону цикла, а при выходе - направление break
-
-        function processQuestionContent(icon) {
-          // Check directly if icon.content is an object with operator: "and"
-          if (typeof icon.content === 'object') {
-            let dirNewItem = null;
-            if (icon.content.operator === "and") {
-              dirNewItem = 'one';
-            } else if (icon.content.operator === "or") {
-              dirNewItem = 'two';
-            } else if (icon.content.operator === "not") {
-              dirNewItem = 'one';
-            }
-
-            if (icon.content.operator === "not") {
-              icon.content = icon.content.operand;
-              icon.flag1 = 0;
-            } else {
-              const newIcon2Id = String(nextNodeId++);
-              const newIcon2 = {
-                ...icon,
-                content: icon.content.right,
-                id: newIcon2Id
-              };
-              icon[dirNewItem] = newIcon2Id;
-              items[newIcon2Id] = newIcon2;
-              icon.content = icon.content.left;
-              processQuestionContent(icon);
-              processQuestionContent(newIcon2);
-              if (!newIcon2.two) {
-                iconsForLink.push({ item: newIcon2, dir: "two" });
-              }
-              if (!newIcon2.one) {
-                iconsForLink.push({ item: newIcon2, dir: "one" });
-              }
-            }
-          }
-          iconsForLink = iconsForLink.filter(itemDir => {
-            // Проверяем, определено ли свойство 'one' или 'two' у иконы
-            if (itemDir.item[itemDir.dir]) {
-              // Если определено, то удаляем элемент (возвращаем false)
-              return false;
-            } else {
-              // Если не определено, то оставляем элемент (возвращаем true)
-              return true;
-            }
-          });
-        }
-
-        var iconOne = icon;
-        var iconTwo = icon;
-
-        icon.flag1 = 1; // по умолчанию в one - yes
-
-        let firstIconIsBreak = false;
-        if (element.no && element.no.length > 0 && element.no[0].type === "break") {
-          firstIconIsBreak = true;
-          icon.flag1 = 0;
-        } else if (element.yes && element.yes.length > 0 && element.yes[0].type === "break") {
-          firstIconIsBreak = true;
-        }
-
-        if (firstIconIsBreak) { // Если первая икона в no или yes - это break тогда
-          iconsForLink.push({ item: iconOne, dir: "one" });
-
-          let iconsLoop = [];
-          if (icon.flag1 === 1) {
-            iconsLoop = processObjects(items, element.no, [{ item: iconTwo, dir: "two" }]);
-          } else {
-            iconsLoop = processObjects(items, element.yes, [{ item: iconTwo, dir: "two" }]);
-          }
-
-          for (const itemDir of iconsLoop) {
-            itemDir.item[itemDir.dir] = firstAddedIconId;
-          }
-          for (const itemDir of firstIconsForLink) {
-            itemDir.item[itemDir.dir] = nextNodeId - 1;
-          }
-        } else {
-          let selectIcon = null;
-
-          function findExistingIcon(items, type, content) {
-            for (const key in items) {
-              const item = items[key];
-              if (item.type === type && item.content === content) {
-                return true;
-              }
-            }
-            return false;
-          }
-
-          if (typeof icon.content === 'object' && icon.content.operator === 'equal') {
-            if (!findExistingIcon(items, 'select', icon.content.left)) {
-              let selectIconId = String(nextNodeId++);
-              selectIcon = { type: 'select', content: icon.content.left };
-              selectIcon.one = icon.id;
-              items[selectIconId] = selectIcon;
-
-              for (const itemDir of firstIconsForLink) {
-                itemDir.item[itemDir.dir] = selectIconId;
-              }
-            }
-
-            icon.type = 'case';
-            icon.content = icon.content.right;
-          }
-
-          if (element.no && element.no.length > 0) {
-            iconsForLink.push(...processObjects(items, element.no, [{ item: iconTwo, dir: "two" }]));
-          } else {
-            iconsForLink.push({ item: iconTwo, dir: "two" });
-          }
-
-          if (element.yes && element.yes.length > 0) {
-            iconsForLink.push(...processObjects(items, element.yes, [{ item: iconOne, dir: "one" }]));
-          } else {
-            iconsForLink.push({ item: iconOne, dir: "one" });
-          }
-        }
-
-        processQuestionContent(icon);
-      }
-
+    function processObjects(items, objectsForProcessing, returnFirst = true) {
+      let lastIcon = null;
+      let firstIcon = null;
       if (!objectsForProcessing || objectsForProcessing.length === 0) {
-      } else if (isArray(objectsForProcessing)) {
-        for (const element of objectsForProcessing) {
-          iconsForLink = processObjects(items, element, iconsForLink);
+        const parent = parentStack[parentStack.length - 1];
+        if (parent) {
+          lastIcon = parent.item;
+          firstIcon = parent.item;
         }
+      } else if (isArray(objectsForProcessing)) {
+        for (let i = 0; i < objectsForProcessing.length; i++) {
+          const element = objectsForProcessing[i];
+          const icon = processObjects(items, element);
+          if (!firstIcon) {
+            firstIcon = icon;
+            const parent = parentStack[parentStack.length - 1];
+            if (parent && icon) {
+              parent.item[parent.dir] = icon.id;
+            }
+          }
+          if (icon && lastIcon && !lastIcon.one) {
+              lastIcon.one = icon.id;
+          }
+          if (icon && (icon.type === 'question' || icon.type === 'loop')) {
+            lastIcon = icon.end;
+          } else {
+            lastIcon = icon;
+          }
+      }
       } else if (isObject(objectsForProcessing)) {
         let element = objectsForProcessing;
-        if (element.type === "address") {
-          processAddress(element);
-        } else if (element.type === "loop") {
-          processLopp(element);
+        if (element.type === "loop") {
+          lastIcon = processLopp(lastIcon, element);
         } else {
-          let newIcon = addIcon(element);
-          iconsForLink = linkIcon(newIcon);
-
+          let newIcon = addIcon(items, element);
           if (element.type === "question") {
-            processQuestion(newIcon, element);
+            lastIcon = processQuestion(newIcon, element);
+          } else {
+            lastIcon = newIcon;
           }
         }
       }
-
-      return iconsForLink;
+      if (returnFirst) {
+        return firstIcon || lastIcon;
+      } else {
+        return lastIcon
+      }
     }
 
-    function exit(iconsForLink) {
+    function addIcon(items, element) {
+      const iconId = String(nextNodeId++);
+      const icon = {
+        content: element.content,
+        type: element.type,
+        id: iconId
+      };
+      if (element.type === "loopend") {
+        icon.type = "arrow-loop";
+        icon.content = "";
+      }
+      if (element.type !== "loop" && element.type !== "loopend") {
+        items[iconId] = icon;
+      }
+      if (element.type === "question") {
+        icon.flag1 = 1; // по умочанию
+      }
+      if (element.type === "endQuestion" || element.type === "endLoop") {
+        icon.content = "";
+      }
+      return icon;
+    }
+
+    function processQuestion(icon, element) {
+      const endQuestionIcon = addIcon(drakon.items, { type: "endQuestion" });
+      icon.end = endQuestionIcon; // Store endQuestionIcon in the question icon
+
+      let lastIconNo = null;
+      if (element.no && element.no.length > 0) {
+        parentStack.push({ item: icon, dir: "two" });
+        lastIconNo = processObjects(drakon.items, element.no, false);
+        parentStack.pop();
+      }
+      if (lastIconNo) {
+        lastIconNo.one = endQuestionIcon.id;
+      } else {
+        icon.two = endQuestionIcon.id;
+      }
+
+      let lastIconYes = null;
+      if (element.yes && element.yes.length > 0) {
+        parentStack.push({ item: icon, dir: "one" });
+        lastIconYes = processObjects(drakon.items, element.yes, false);
+        parentStack.pop();
+      }
+      if (lastIconYes) {
+        lastIconYes.one = endQuestionIcon.id;
+      } else {
+        icon.one = endQuestionIcon.id;
+      }
+
+      processQuestionContent(icon);
+
+      return icon; // Return the original question icon
+    }
+
+    function processQuestionContent(icon) {
+      // Check directly if icon.content is an object with operator: "and"
+      if (typeof icon.content === 'object') {
+        let dirNewItem = null;
+        if (icon.content.operator === "and") {
+          dirNewItem = 'one';
+        } else if (icon.content.operator === "or") {
+          dirNewItem = 'two';
+        } else if (icon.content.operator === "not") {
+          dirNewItem = 'one';
+        }
+
+        if (icon.content.operator === "not") {
+          icon.content = icon.content.operand;
+          icon.flag1 = 0;
+        } else {
+          //const newIcon2Id = String(nextNodeId++);
+          const newIcon2 = addIcon(drakon.items, { type: "question", content: icon.content.right });
+
+          newIcon2.one = icon.one;
+          newIcon2.two = icon.two;
+          icon[dirNewItem] = newIcon2.id;
+          icon.content = icon.content.left;
+          processQuestionContent(icon);
+          processQuestionContent(newIcon2);
+        }
+      }
+    }
+
+    function processLopp(icon, element) {
+       const endLoopIcon = addIcon(drakon.items, { type: "endLoop" });
+       if (icon) {
+        icon.end = endLoopIcon;
+       }
+
+
+      let lastIconBody = null;
+      if (icon) {
+        parentStack.push({ item: icon, dir: "one" });
+        lastIconBody = processObjects(drakon.items, element.body);
+        parentStack.pop();
+        if (lastIconBody) {
+          lastIconBody.one = endLoopIcon.id;
+        } else {
+          icon.one = endLoopIcon.id;
+        }
+      } else {
+        const parent = parentStack[parentStack.length - 1];
+        if (parent) {
+          parent.item[parent.dir] = endLoopIcon.id;
+        }
+      }
+      return icon;
+    }
+
+    function exit() {
       let endNodeId = null;
       for (const key in drakon.items) {
         const item = drakon.items[key];
@@ -280,24 +240,50 @@ function astToDrakon(astJson) {
         }
       }
 
-      if (!endNodeId) {
-        endNodeId = String(nextNodeId++);
-        drakon.items[endNodeId] = { type: "end" };
-      }
-
-      for (const itemDir of iconsForLink) {
-        if (!itemDir.item[itemDir.dir]) {
-          itemDir.item[itemDir.dir] = endNodeId;
+      const endQuestionIconIds = [];
+      const endLoopIconIds = [];
+      for (const key in drakon.items) {
+        const item = drakon.items[key];
+        if (item.type === "endQuestion") {
+          endQuestionIconIds.push(key);
+        }
+        if (item.type === "endLoop") {
+          endLoopIconIds.push(key);
         }
       }
 
       for (const key in drakon.items) {
         const item = drakon.items[key];
+        while (item.one && endQuestionIconIds.includes(item.one)) {
+          item.one = drakon.items[item.one].one;
+        }
+        while (item.one && endQuestionIconIds.includes(item.one)) {
+          item.two = drakon.items[item.one].one;
+        }
+        while (item.one && endLoopIconIds.includes(item.one)) {
+          item.one = drakon.items[item.one].one;
+        }
+      }
+
+      for (const key of endQuestionIconIds) {
+        delete drakon.items[key];
+      }
+      for (const key of endLoopIconIds) {
+        delete drakon.items[key];
+      };
+
+      if (!endNodeId) {
+        endNodeId = String(nextNodeId++);
+        drakon.items[endNodeId] = { type: "end" };
+      };
+
+      for (const key in drakon.items) {
+        const item = drakon.items[key];
         if (item.type === "question") {
-          if (item.one === null) {
+          if (!item.one) {
             item.one = endNodeId;
           }
-          if (item.two === null) {
+          if (!item.two) {
             item.two = endNodeId;
           }
         }
@@ -306,15 +292,10 @@ function astToDrakon(astJson) {
             item.one = endNodeId;
           }
         }
-        if (item.type === "end") {
-          delete item.one;
-          delete item.two;
-        }
       }
     }
 
-    const iconsForLink = processBranches();
-    exit(iconsForLink);
+    processBranches();
 
     return {
       fileName: ast.name,
