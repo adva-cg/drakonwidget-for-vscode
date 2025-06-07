476749549a410934a4d74529f8a0591b447aba2d
Adadurov Vitaly <v.adadurov@smarthorizon.ru>
2025-06-07 07:27:08 +0500

    Что-то из предыдущего

--- src/drakongen/src/structToDrakon.d.ts
+++
@@ -1,5 +0,0 @@
-declare module './structToDrakon.js' {
-  export function astToDrakon(astJson: string): { fileName: string; content: string; };
-  export function generateDrakonFiles(inputDir: string, outputDir: string): void;
-  // Add other exported functions if any
-}
